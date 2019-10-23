d78f113f57039acbfe3fb2714131e9b703645f28
Phil Davidson <pxdavidson@msn.com>
2019-10-23 21:16:23 +0100

    Added navmesh and basic move script

---
+++ udemy_rpgCombat/Assets/Scripts/Mover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Variables
+    [SerializeField] Transform target;
+
+    // Cache
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.SetDestination(target.position);
+    }
+}
